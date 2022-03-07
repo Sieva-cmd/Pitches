1ea4b9910eb7ff69e2ad819b3d7d9eb8fdf8b693
Sieva-cmd <musyokasieva@gmail.com>
2022-03-07 19:36:27 +0300

    add user profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..import db
+from ..models import User
 from flask_login import login_required
 
 
@@ -14,5 +15,15 @@ def index():
     '''
     title ='Pitches'
     return render_template('index.html', title=title)
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)    
 
 
--- app/models.py
+++ app/models.py
@@ -19,6 +19,8 @@ class User(UserMixin,db.Model):
     username =db.Column(db.String(255), index=True)
     pass_secure = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     
     
     @property
@@ -37,12 +39,4 @@ class User(UserMixin,db.Model):
         return f'User{self.username}'
 
 
-# class User(db.Model):
-#     __tablename__ ='users'
-#     id = db.Column(db.Integer,primary_key = True)
-#     username = db.Column(db.String(255),index =True)
-   
-   
-#     def __repr__(self):
-#         return f'User {self.username}'
 
